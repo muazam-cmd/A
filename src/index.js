e324e422b1eff59369e7b8ecfc544be9ad595dc1
muazam-cmd <muazamshahid7@gmail.com>
2023-01-26 14:12:36 +0400

    Showing some basic content and removing unnecessary Files

--- src/index.js
+++ src/index.js
@@ -1,17 +1,22 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+// 1) Import React and React Dom Libraries 
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// 2) Get a reference to the div with ID root
+
+const el = document.getElementById('root');
+
+// 3) Tell a  React  to take control of that element
+
+const root = ReactDOM.createRoot(el);
+
+// 4) Create a Component
+
+function App() {
+    return <h1>Hey There!</h1>
+}
+
+// 5) Show the Component on the Screen
+root.render(<App />);
+
